cc6f029ac8f310f1881e3a8a5fe17efae62d1e0d
Stephano George <huohuo5230@gmail.com>
2020-11-23 22:10:51 +0800

    change the package name

---
+++ iciba_api/__init__.py
@@ -0,0 +1,1 @@
+name = 'iciba_word'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name='iciba_word_looking_up_SDK',
+    name='iciba_word-Stephano',
     version='0.0.1',
     author='Stephano George',
     author_email='huohuo5230@gmail.com',
