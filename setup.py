e6fa1d310e71e0ff7b3c4c91b71752a8bdf05880
Stephano George <huohuo5230@gmail.com>
2020-11-23 21:23:55 +0800

    make as a package

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='iciba-word-looking-up-SDK-Stephano-George',
+    version='0.0.1',
+    author='Stephano George',
+    author_email='huohuo5230@gmail.com',
+    description='iciba.com word looking up SDK',
+    url='https://github.com/StephanoGeorge/iciba-API',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
